6606b63474e15117603d5a14c5d61c8be6d904e1
FichteFoll <fichtefoll2@googlemail.com>
2016-12-27 00:03:25 +0100

    Checks for .no-sublime-package file

---
+++ package_reviewer/checkers/check_no-sublime-package.py
@@ -0,0 +1,17 @@
+from ..file import FileChecker
+
+
+class CheckMessages(FileChecker):
+
+    def check(self):
+        exists = self.sub_path(".no-sublime-package").is_file()
+        if not exists:
+            return
+
+        potential_invokers = self.globs("*.py", "**/*.sublime-build")
+        if next(potential_invokers, None) is None:
+            self.fail("'.no-sublime-package' is defined, "
+                      "but no other resource file can make use of it")
+        else:
+            self.warn("'.no-sublime-package' is defined. "
+                      "Please verify that it is *really* necessary")
