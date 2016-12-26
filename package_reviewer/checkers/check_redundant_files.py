fa6fede086f918604134a3ac4ed07066afecda49
FichteFoll <fichtefoll2@googlemail.com>
2016-12-26 23:11:18 +0100

    Check for .pyc files

--- package_reviewer/checkers/check_redundant_files.py
+++ package_reviewer/checkers/check_redundant_files.py
@@ -11,3 +11,16 @@ class CheckPackageMetadata(FileChecker):
         if self.sub_path("package-metadata.json").is_file():
             self.fail("'package-metadata.json' is supposed to be automatically generated "
                       "by Package Control during installation")
+
+
+class CheckPycFiles(FileChecker):
+
+    def check(self):
+        pyc_files = self.glob("**/*.pyc")
+        if not pyc_files:
+            return
+
+        for path in pyc_files:
+            if path.with_suffix(".py").is_file():
+                self.fail("'{}' is redundant because its corresponding .py file exists"
+                          .format(self.rel_path(path)))
