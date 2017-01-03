e0063c0d5604372c1a07a179f5206a0a27570817
FichteFoll <fichtefoll2@googlemail.com>
2017-01-03 02:08:07 +0100

    Change message of semver tag check

--- package_reviewer/check/repo/check_semver_tags.py
+++ package_reviewer/check/repo/check_semver_tags.py
@@ -8,9 +8,9 @@ class CheckSemverTags(RepoChecker):
 
     def check(self):
         if not self.semver_tags:
-            msg = "No semantic version tags found. See http://semver.org."
+            msg = "No semantic version tags found"
             for tag in self.tags:
                 if re.search(r"(v|^)\d+\.\d+$", tag.name):
-                    msg += " Semantic versions consist of exactly three numeric parts."
+                    msg += " (semantic versions consist of exactly three numeric parts)"
                     break
             self.fail(msg)
