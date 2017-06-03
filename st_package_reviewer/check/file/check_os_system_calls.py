dbfa1598d7c8de55f9fac95199d827363741805a
Raoul Wols <raoulwols@gmail.com>
2017-06-03 22:31:25 +0200

    Add AST checks: command names, os.system calls and platform package
    The check_command_names module does three checks on any command that it
    finds:
    
    1. It checks if the command is CamelCase. Issues a warning if not.
    2. It checks for a common prefix for all commands; this prevents
       cluttering of command names. Issues a warning if multiple
       prefixes are found.
    3. It checks if the command ends with the word "Command" and issues
       a warning if not.
    
    The check_os_system_calls module checks if there's any call to
    os.system and suggests using subprocess.check_output instead via a
    warning.
    
    The check_platform_usage checks for importing the platform package and
    suggests to use sublime.platform() and sublime.arch() instead via a
    warning.
    
    Failure to parse a pyfile into an AST is considered an error and so
    I issue an error in that case.

---
+++ st_package_reviewer/check/file/check_os_system_calls.py
@@ -0,0 +1,31 @@
+from . import FileChecker
+import ast
+
+class CheckOsSytemCalls(FileChecker):
+    """Checks for any calls to os.system and suggests to use subprocess.check_call intead."""
+
+    def check(self):
+        self.counter = 0
+        self.visit_all_pyfiles()
+        if self.counter > 0:
+            self.warn("Detected os.system usage: Make sure you thought about the platform key in "
+                "your pull request.")
+
+    def _warn_about_os_system(self, node):
+        self.counter += 1
+        with self.file_context(self.current_file):
+            self.warn("At line {}, column {}, consider replacing os.system with "
+                "subprocess.check_output, or use sublime's Default.exec.ExecCommand.".format(
+                    node.lineno, node.col_offset))
+
+    def visit_Call(self, node):
+        try:
+            attr = node.func.attr
+            id = node.func.value.id
+        except Exception as e:
+            return
+        if id == "os" and attr == "system":
+            self._warn_about_os_system(node)
+
+
+
---
+++ st_package_reviewer/check/file/check_platform_usage.py
@@ -0,0 +1,40 @@
+from . import FileChecker
+import ast
+
+class CheckPlatformUsage(FileChecker):
+    """Checks wether the plugin uses the platform package and warn about it. Suggests to use 
+    sublime.platform() and sublime.arch(). If it uses the platform package and/or 
+    sublime.platform(), checks if the commit has a platform key. If not, warn about it."""
+
+    def check(self):
+        self.counter = 0
+        self.visit_all_pyfiles()
+        if self.counter > 0:
+            self.warn("Detected platform usage: Make sure you thought about the platform key in "
+                "your pull request.")
+
+    def _warn_platform_module_usage(self, node):
+        self.counter += 1
+        with self.file_context(self.current_file):
+            self.warn("At line {}, column {}, consider replacing the platform module by "
+                "using sublime.platform() and sublime.arch()".format(node.lineno, node.col_offset))
+
+    def visit_Import(self, node):
+        for alias_node in node.names:
+            name = alias_node.name
+            if name == "platform":
+                self._warn_platform_module_usage(node)
+
+    def visit_ImportFrom(self, node):
+        if node.module == "platform":
+            self._warn_platform_module_usage(node)
+
+    def visit_Call(self, node):
+        try:
+            attr = node.func.attr
+            id = node.func.value.id
+        except Exception as e:
+            return
+        if id == "sublime" and attr in ("platform", "arch"):
+            self.counter += 1
+
