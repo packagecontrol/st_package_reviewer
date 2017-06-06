5d87e9b822db815fa9e03d74b413a46b3e6e9d3a
Raoul Wols <raoulwols@gmail.com>
2017-06-06 16:19:34 +0200

    Delete ParseFailures test; add back later

--- tests/packages/ParseFailures/invalidsyntax.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-
-class AwesomeFooCommand(sublime_plugin.ApplicationCommand)
-	"""I'm missing a colon"""
-	
-	def run(self):
-		print("Hello, world!")
--- tests/packages/ParseFailures/python2file.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-
-class AwesomeFooCommand(sublime_plugin.ApplicationCommand):
-
-	def run(self):
-		# Used to be okay in Python2; no longer the case in Python3.
-		print "Hello, world!"
