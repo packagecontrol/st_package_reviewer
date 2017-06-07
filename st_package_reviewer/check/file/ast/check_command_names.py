4332fc05683e0b936f21a46fb33b950512678d48
FichteFoll <fichtefoll2@googlemail.com>
2017-06-07 02:55:27 +0200

    Extract stateless function from class

--- st_package_reviewer/check/file/ast/check_command_names.py
+++ st_package_reviewer/check/file/ast/check_command_names.py
@@ -3,6 +3,27 @@ import re
 import ast
 
 
+# TODO: This only checks immediate base classes; need more traversing for deeper-derived base
+# classes.
+def _is_derived_from_command(node):
+    interesting = ("TextCommand", "WindowCommand", "ApplicationCommand", "ExecCommand")
+    for base in node.bases:
+        if isinstance(base, ast.Attribute):
+            # something of the form module_name.BaseClassName
+            if isinstance(base.value, ast.Attribute):
+                if base.value.value.id == "Default":
+                    # Something derived from a class in Default... Must be ExecCommand
+                    return True
+            elif isinstance(base.value, ast.Name):
+                if base.value.id == "sublime_plugin" and base.attr in interesting:
+                    return True
+        elif isinstance(base, ast.Name):
+            # something of the form BaseClassName
+            if base.id in interesting:
+                return True
+    return False
+
+
 class CheckCommandNames(AstChecker):
     """Finds all sublime commands and does various checks on them."""
 
@@ -15,28 +36,8 @@ class CheckCommandNames(AstChecker):
                       " so as to not clutter the command namespace."
                       .format(", ".join(sorted(self.prefixes))))
 
-    # TODO: This only checks immediate base classes; need more traversing for deeper-derived base
-    # classes.
-    def is_derived_from_command(self, node):
-        interesting = ("TextCommand", "WindowCommand", "ApplicationCommand", "ExecCommand")
-        for base in node.bases:
-            if isinstance(base, ast.Attribute):
-                # something of the form module_name.BaseClassName
-                if isinstance(base.value, ast.Attribute):
-                    if base.value.value.id == "Default":
-                        # Something derived from a class in Default... Must be ExecCommand
-                        return True
-                elif isinstance(base.value, ast.Name):
-                    if base.value.id == "sublime_plugin" and base.attr in interesting:
-                        return True
-            elif isinstance(base, ast.Name):
-                # something of the form BaseClassName
-                if base.id in interesting:
-                    return True
-        return False
-
     def visit_ClassDef(self, node):
-        if not self.is_derived_from_command(node):
+        if not _is_derived_from_command(node):
             return
 
         with self.node_context(node):
